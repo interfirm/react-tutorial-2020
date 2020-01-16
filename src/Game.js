05dbd2272d72b20c22bab1f4c16114e03e42c228
rnasakazu <45251394+rnasakazu@users.noreply.github.com>
2020-01-16 10:36:38 +0900

    [WIP] Refactor to function component (#4)) (#8)
    * [WIP] Refactor to function component (#4))
    
    * Refactor board class to function component (#4)
    
    * Refactor game class to function component (#4)
    
    * Fix missing semicolon (#4)
    
    * Refactor board class to function component (#4)
    
    * Refactor game class to function component (#4)
    
    * Fix missing semicolon (#4)
    
    * Delete unnecessary variables (#4)
    
    * Modify with eslint (#4)

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+
+Square.propTypes = {
+  isWinLine: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+}
+
+Board.propTypes = {
+  squares: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
+  winLine: PropTypes.array.isRequired,
+  index: PropTypes.number.isRequired,
+  isWinLine: PropTypes.bool.isRequired,
+}
 
 function Square(props) {
   return (
-    <button className= {`square ${props.isWinLine ? "highlight" : ""}`} onClick={props.onClick}>
+    <button className={`square ${props.isWinLine ? "highlight" : ""}`} onClick={props.onClick}>
       {props.value}
     </button>
-  );
+  )
 }
 
 function compareFunc(a, b) {
-  return a - b;
+  return a - b
 }
 
 function calculateWinner(squares) {
@@ -22,161 +37,145 @@ function calculateWinner(squares) {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
-  let winLine = [];
+  ]
+  let winLine = []
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+    const [a, b, c] = lines[i]
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      winLine = winLine.concat(lines[i]);
+      winLine = winLine.concat(lines[i])
     }
   }
   if (winLine.length) {
-    winLine = winLine.filter((x, i, self) => self.indexOf(x) === i);
-    winLine.sort(compareFunc);
+    winLine = winLine.filter((x, i, self) => self.indexOf(x) === i)
+    winLine.sort(compareFunc)
     return {
       winner: squares[winLine[0]],
       winLine,
-    };
+    }
   }
   return {
     winner: null,
     winLine: null,
-  };
+  }
 }
 
-class Board extends React.Component {
-  renderSquare({index, isWinLine} = {}) {
+function Board(props) {
+  const renderSquare = ({ index, isWinLine } = {}) => {
     return (
       <Square
-        key = {index}
-        value = {this.props.squares[index]}
-        onClick = {() => this.props.onClick(index)}
-        isWinLine = {isWinLine}
+        key={index}
+        value={props.squares[index]}
+        onClick={() => props.onClick(index)}
+        isWinLine={isWinLine}
       />
-    );
+    )
   }
 
-  render() {
-    let board = [];
+  let board = []
 
-    let index = 0;
-    for (let i = 0; i < 9; i++) {
-      if ([0, 3, 6].includes(i)) {
-        board.push(<div className="board-row" key={`b_${i}`}></div>)
-      }
-
-      if (this.props.winLine && i === this.props.winLine[index]) {
-        board.push(this.renderSquare({index: i, isWinLine: true}));
-        index++;
-      } else {
-        board.push(this.renderSquare({index: i, isWinLine: false}));
-      }
+  let index = 0
+  for (let i = 0; i < 9; i++) {
+    if ([0, 3, 6].includes(i)) {
+      board.push(<div className="board-row" key={`b_${i}`}></div>)
     }
 
-    return (
-      <div>{board}</div>
-    );
+    if (props.winLine && i === props.winLine[index]) {
+      board.push(renderSquare({ index: i, isWinLine: true }))
+      index++
+    } else {
+      board.push(renderSquare({ index: i, isWinLine: false }))
+    }
   }
+
+  return <div>{board}</div>
 }
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-        col: null,
-        row: null,
-      }],
-      isHistoryDescending: false,
-      stepNumber: 0,
-      xIsNext: true,
-    }
-  }
+function Game() {
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+      col: null,
+      row: null,
+    },
+  ])
+  const [isHistoryDescending, setIsHistoryDescending] = useState(false)
+  const [stepNumber, setStepNumber] = useState(0)
+  const [xIsNext, setXIsNext] = useState(true)
 
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    const col = Math.floor(i / 3);
-    const row = i % 3;
+  const handleClick = i => {
+    const newHistory = history.slice(0, stepNumber + 1)
+    const current = newHistory[newHistory.length - 1]
+    const squares = current.squares.slice()
+    const col = Math.floor(i / 3)
+    const row = i % 3
 
     if (calculateWinner(squares).winner || squares[i]) {
-      return;
+      return
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares,
-        col,
-        row,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
+    squares[i] = xIsNext ? "X" : "O"
+    setHistory(
+      newHistory.concat([
+        {
+          squares,
+          col,
+          row,
+        },
+      ])
+    )
+    setStepNumber(newHistory.length)
+    setXIsNext(!xIsNext)
   }
 
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
+  const jumpTo = step => {
+    setStepNumber(step)
+    setXIsNext(step % 2 === 0)
   }
 
-  sortHistory() {
-    this.setState({
-      isHistoryDescending: !this.state.isHistoryDescending,
-    });
+  const sortHistory = () => {
+    setIsHistoryDescending(!isHistoryDescending)
   }
 
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winInfo = calculateWinner(current.squares);
+  const current = history[stepNumber]
+  const winInfo = calculateWinner(current.squares)
 
-    const moves = history.map((step, move) => {
-      const col = step.col;
-      const row = step.row;
-      let desc = move ?
-        'Go to #' + move +'(' + col + ',' + row + ')':
-        'Go to game start';
-      if (this.state.stepNumber === move) {
-        desc = <b>{desc}</b>
-      }
-
-      return (
-        <li key={move}>
-          <button onClick = {() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winInfo.winner) {
-      status = 'Winner: ' + winInfo.winner;
-    } else if (this.state.stepNumber === 9) {
-      status = 'Draw';
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+  const moves = history.map((step, move) => {
+    const col = step.col
+    const row = step.row
+    let desc = move ? `Go to #${move} (${col},${row})` : "Go to game start"
+    if (stepNumber === move) {
+      desc = <b>{desc}</b>
     }
 
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares = {current.squares}
-            onClick = {(i) => this.handleClick(i)}
-            winLine = {winInfo.winLine}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <div><button onClick = {() => this.sortHistory()}>表示切り替え</button></div>
-          {this.state.isHistoryDescending ? <ol reversed="reversed">{moves.reverse()}</ol> : <ol>{moves}</ol>}
-        </div>
-      </div>
-    );
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    )
+  })
+
+  let status
+  if (winInfo.winner) {
+    status = `Winner: ${winInfo.winner}`
+  } else if (stepNumber === 9) {
+    status = "Draw"
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`
   }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={i => handleClick(i)} winLine={winInfo.winLine} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <div>
+          <button onClick={() => sortHistory()}>表示切り替え</button>
+        </div>
+        {isHistoryDescending ? <ol reversed="reversed">{moves.reverse()}</ol> : <ol>{moves}</ol>}
+      </div>
+    </div>
+  )
 }
 
 export default Game
